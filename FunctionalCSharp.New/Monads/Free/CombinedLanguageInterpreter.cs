6341add5ec79327e22da1df896e3dd70e0dbc3f3
Michal <m.wudarczyk@hotmail.com>
2024-11-21 14:49:20 +0100

    Make CombinedLanguageInterpreter inherit InterpreterBase

--- FunctionalCSharp.New/Monads/Free/CombinedLanguageInterpreter.cs
+++ FunctionalCSharp.New/Monads/Free/CombinedLanguageInterpreter.cs
@@ -5,17 +5,13 @@ namespace FunctionalCSharp.New.Monads.Free;
 public class CombinedLanguageInterpreter<TOutput, TLanguageA, TLanguageB>(
     FreeInterpreterBase<TOutput, TLanguageA> languageAInterpreter,
     FreeInterpreterBase<TOutput, TLanguageB> languageBInterpreter
-) where TLanguageA : IFunctor<TLanguageA> where TLanguageB : IFunctor<TLanguageB>
+) :FreeInterpreterBase<TOutput,Coproduct<TLanguageA,TLanguageB>>
+    where TLanguageA : IFunctor<TLanguageA> where TLanguageB : IFunctor<TLanguageB>
 {
-    private TNext InterpretSingle<TNext>(IKind<Coproduct<TLanguageA, TLanguageB>, TNext> instruction)
+    public override TNext InterpretSingle<TNext>(IKind<Coproduct<TLanguageA, TLanguageB>, TNext> instruction)
     {
         return instruction.To().Match(
             languageAInterpreter.InterpretSingle,
             languageBInterpreter.InterpretSingle);
-    }
-
-    public TOutput Interpret(Free<Coproduct<TLanguageA, TLanguageB>, TOutput> program)
-    {
-        return Free<Coproduct<TLanguageA, TLanguageB>>.Iter(InterpretSingle, program);
     }
 }
