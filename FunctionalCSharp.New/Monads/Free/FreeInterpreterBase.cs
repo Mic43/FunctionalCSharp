c67be4fb4fb000962a612986d43908bb3fa33999
Michal <m.wudarczyk@hotmail.com>
2024-11-21 14:47:21 +0100

    move to separate file

--- FunctionalCSharp.New/Monads/Free/FreeInterpreterBase.cs
+++ FunctionalCSharp.New/Monads/Free/FreeInterpreterBase.cs
@@ -12,21 +12,3 @@ public abstract class FreeInterpreterBase<TOutput, Functor> : IFreeInterpreter<T
 
     public abstract TNext InterpretSingle<TNext>(IKind<Functor, TNext> command);
 }
-
-public class CombinedLanguageInterpreter<TOutput, TLanguageA, TLanguageB>(
-    FreeInterpreterBase<TOutput, TLanguageA> languageAInterpreter,
-    FreeInterpreterBase<TOutput, TLanguageB> languageBInterpreter
-) where TLanguageA : IFunctor<TLanguageA> where TLanguageB : IFunctor<TLanguageB>
-{
-    private TNext InterpretSingle<TNext>(IKind<Coproduct<TLanguageA, TLanguageB>, TNext> instruction)
-    {
-        return instruction.To().Match(
-            languageAInterpreter.InterpretSingle,
-            languageBInterpreter.InterpretSingle);
-    }
-
-    public TOutput Interpret(Free<Coproduct<TLanguageA, TLanguageB>, TOutput> program)
-    {
-        return Free<Coproduct<TLanguageA, TLanguageB>>.Iter(InterpretSingle, program);
-    }
-}
