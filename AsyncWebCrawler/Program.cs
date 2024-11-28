33f88de81a57a62af20ed010d11ab0ff63cb5503
Michal <m.wudarczyk@hotmail.com>
2024-11-28 13:04:38 +0100

    Added support for exceptions when downloading urls

--- AsyncWebCrawler/Program.cs
+++ AsyncWebCrawler/Program.cs
@@ -5,19 +5,23 @@ using FunctionalCSharp.New.Monads;
 using FunctionalCSharp.New.Monads.ListT;
 using HtmlAgilityPack;
 using static FunctionalCSharp.New.Utils;
+using static FunctionalCSharp.New.MonadicConditionals;
+
 
 ListT<Async, (string, string)> Crawl(string url)
 {
     HashSet<string> visited = new();
     HttpClient client = new HttpClient();
     var doc = new HtmlDocument();
-    
+
     ListT<Async, (string link, string title)> Execute(string url)
     {
         FunctionalCSharp.New.Monads.List<string> ExtractLinks(string html)
         {
+            if (string.IsNullOrEmpty(html))
+                return List.Empty<string>().To();
             var hrefLinks = new System.Collections.Generic.List<string>();
-            
+
             // Load the HTML into an HtmlDocument object
             doc.LoadHtml(html);
 
@@ -26,30 +30,35 @@ ListT<Async, (string, string)> Crawl(string url)
 
             if (anchorTags == null) return new FunctionalCSharp.New.Monads.List<string>(hrefLinks);
             hrefLinks.AddRange(anchorTags.Select(anchor => anchor.GetAttributeValue("href", string.Empty))
-                .Where(href => !string.IsNullOrEmpty(href) && href.Trim().StartsWith(@"https://")));
+                .Where(href =>
+                    !string.IsNullOrEmpty(href) &&
+                    href.Trim().StartsWith(@"https://")));
 
             return new FunctionalCSharp.New.Monads.List<string>(hrefLinks);
         }
-        
+
         string? ExtractTitle(string html)
         {
+            if (string.IsNullOrEmpty(html))
+                return "";
             doc.LoadHtml(html);
             var titleNode = doc.DocumentNode.SelectSingleNode("//title");
             return titleNode?.InnerText;
         }
-        
+
         return
             from _ in When(visited.Add(url),
-                    from html in ListT<Async>.Lift(client.GetStringAsync(url).ToAsync()).To()
-                    from link in 
-                        List.MSum(from c in ExtractLinks(html)
-                                  select ListT<Async>.Pure(c)).To()
-                    let title = ExtractTitle(html)
+                    from html in ListT<Async>.Lift(client.GetStringAsync(url)
+                        .ToAsync().Catch()).To()
+                    from link in
+                        List.MSum(from c in ExtractLinks(html.DefaultIfError(""))
+                            select ListT<Async>.Pure(c)).To()
+                    let title = ExtractTitle(html.DefaultIfError(""))
                     from res in Execute(link).To() + ListT<Async>.Pure((link, title)).To()
-                    select res)
+                    select res
+                )
                 .To()
             select _;
-        
     }
 
     return Execute(url);
